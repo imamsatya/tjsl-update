64107423d2bbd2a45a37420f22712a610d66eec7
didi.rosidi <didi.rosidi@bumn.go.id>
2021-09-30 11:25:50 +0700

    revisi btn search

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -211,7 +211,8 @@
                     <!--begin::Search-->
                     <div class="d-flex align-items-center position-relative my-1" data-kt-view-roles-table-toolbar="base">
 
-                        <button type="button" class="btn btn-active btn-info btn-sm btn-icon btn-search cls-search"  data-toggle="tooltip" title="Cari Data" style="font-size: 16px;"><i class="bi bi-search fs-3"></i></button> &nbsp
+                        <button type="button" class="btn btn-active btn-info btn-sm btn-icon btn-search cls-search btn-search-active" style="margin-right:3px;" data-toggle="tooltip" title="Cari Data"><i class="bi bi-search fs-3"></i></button>
+                        <button type="button" class="btn btn-active btn-light btn-sm btn-icon btn-search cls-search btn-search-unactive" style="display:none;margin-right:3px;" data-toggle="tooltip" title="Cari Data"><i class="bi bi-search fs-3"></i></button>
 
                     </div>
                     <!--end::Search-->
@@ -333,9 +334,17 @@
         updatechartmb();
         
         $('#form-cari').hide();
-        $('body').on('click','.btn-search',function(){
+        $('body').on('click','.btn-search-active',function(){
+            $('.btn-search-active').hide();
+            $('.btn-search-unactive').show();
             $('#form-cari').toggle(600);
         });
+
+        $('body').on('click','.btn-search-unactive',function(){
+            $('.btn-search-active').show();
+            $('.btn-search-unactive').hide();
+            $('#form-cari').toggle(600);
+        }); 
 
 
     });
--- resources/views/pumk/data_mitra/index.blade.php
+++ resources/views/pumk/data_mitra/index.blade.php
@@ -33,7 +33,8 @@
                 <div class="card-toolbar">
                     <!--begin::Search-->
                     <div class="d-flex align-items-center position-relative my-1" data-kt-view-roles-table-toolbar="base">
-                        <button type="button" class="btn btn-active btn-info btn-sm btn-icon btn-search cls-search"  data-toggle="tooltip" title="Cari Data"><i class="bi bi-search fs-3"></i></button> &nbsp
+                        <button type="button" class="btn btn-active btn-info btn-sm btn-icon btn-search cls-search btn-search-active" style="margin-right:3px;" data-toggle="tooltip" title="Cari Data"><i class="bi bi-search fs-3"></i></button>
+                        <button type="button" class="btn btn-active btn-light btn-sm btn-icon btn-search cls-search btn-search-unactive" style="display:none;margin-right:3px;" data-toggle="tooltip" title="Cari Data"><i class="bi bi-search fs-3"></i></button>
                         <button type="button" class="btn btn-warning btn-sm btn-icon cls-export"  data-toggle="tooltip" title="Download Excel"><i class="bi bi-file-excel fs-3"></i></button>
                     </div>
                     <!--end::Search-->
@@ -287,10 +288,17 @@
             exportExcel();
         });
 
-        $('body').on('click','.btn-search',function(){
+        $('body').on('click','.btn-search-active',function(){
+            $('.btn-search-active').hide();
+            $('.btn-search-unactive').show();
             $('#form-cari').toggle(600);
         });
-        
+
+        $('body').on('click','.btn-search-unactive',function(){
+            $('.btn-search-active').show();
+            $('.btn-search-unactive').hide();
+            $('#form-cari').toggle(600);
+        });        
         
         // if("{{ $admin_bumn }}"){
         //     $('.cls-export').hide();
