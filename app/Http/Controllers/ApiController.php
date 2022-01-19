94c169279874003f5d18b1dce5b597ba58a1e78b
didi.rosidi <didi.rosidi@bumn.go.id>
2022-01-19 15:15:44 +0700

    api referensi

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,175 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+use App\Models\Perusahaan;
+use App\Models\Provinsi;
+use App\Models\Kota;
+use App\Models\Tpb;
+use App\Models\PilarPembangunan;
+use App\Models\KodeTujuanTpb;
+use App\Models\KodeIndikator;
+
+
+class ApiController extends Controller
+{
+    public function getprovinsi(Request $request)
+    {
+        $ip = $request->getClientIp();
+        $param = '127.0.0.1';
+
+        if($ip !== $param){
+            $result = "Forbidden Access!";
+        
+            return response()->json(['message' => $result]);            
+        }else{
+            $select = [
+                'id','nama AS provinsi','tgl_sinkronisasi'
+            ];
+    
+            $result = Provinsi::select($select)->where('is_luar_negeri','f')->get();
+    
+            return response()->json(['status' => 1, 'message' => 'OK', 'data' => $result]);
+        }
+    }
+
+    public function getkota(Request $request)
+    {
+        $ip = $request->getClientIp();
+        $param = '127.0.0.1';
+
+        if($ip !== $param){
+            $result = "Forbidden Access!";
+        
+            return response()->json(['message' => $result]);            
+        }else{
+            $select = [
+                'kotas.id AS kota_id','kotas.nama AS kota', 'kotas.provinsi_id','provinsis.nama AS provinsi','kotas.tgl_sinkronisasi'
+            ];
+    
+            $result = Kota::select($select)
+                    ->leftjoin('provinsis','provinsis.id','=','kotas.provinsi_id')
+                    ->where('kotas.is_luar_negeri','f')->get();
+    
+            return response()->json(['status' => 1, 'message' => 'OK', 'data' => $result]);
+        }
+    }
+
+    public function getreferensibumnaktif(Request $request)
+    {
+        $ip = $request->getClientIp();
+        $param = '127.0.0.1';
+
+        if($ip !== $param){
+            $result = "Forbidden Access!";
+        
+            return response()->json(['message' => $result]);            
+        }else{
+            $select = [
+                '*'
+            ];
+    
+            $result = Perusahaan::select($select)
+                    ->where('is_active','t')->get();
+    
+            return response()->json(['status' => 1, 'message' => 'OK', 'data' => $result]);
+        }
+    }    
+
+    public function getreferensitpb(Request $request)
+    {
+        $ip = $request->getClientIp();
+        $param = '127.0.0.1';
+
+        if($ip !== $param){
+            $result = "Forbidden Access!";
+        
+            return response()->json(['message' => $result]);            
+        }else{
+            $select = [
+                "id",
+                "no_tpb",
+                "nama",
+                "keterangan"
+            ];
+    
+            $result = Tpb::select($select)->get();
+    
+            return response()->json(['status' => 1, 'message' => 'OK', 'data' => $result]);
+        }
+    }
+
+    public function getreferensipilar(Request $request)
+    {
+        $ip = $request->getClientIp();
+        $param = '127.0.0.1';
+
+        if($ip !== $param){
+            $result = "Forbidden Access!";
+        
+            return response()->json(['message' => $result]);            
+        }else{
+            $select = [
+                "id",
+                "nama",
+                "keterangan"
+            ];
+    
+            $result = PilarPembangunan::select($select)->get();
+    
+            return response()->json(['status' => 1, 'message' => 'OK', 'data' => $result]);
+        }
+    }
+
+    public function getreferensikodetujuantpb(Request $request)
+    {
+        $ip = $request->getClientIp();
+        $param = '127.0.0.1';
+
+        if($ip !== $param){
+            $result = "Forbidden Access!";
+        
+            return response()->json(['message' => $result]);            
+        }else{
+            $select = [
+                "id",
+                "kode",
+                "keterangan"
+            ];
+    
+            $result = KodeTujuanTpb::select($select)->get();
+    
+            return response()->json(['status' => 1, 'message' => 'OK', 'data' => $result]);
+        }
+    }
+
+
+    public function getreferensikodeindikator(Request $request)
+    {
+        $ip = $request->getClientIp();
+        $param = '127.0.0.1';
+
+        if($ip !== $param){
+            $result = "Forbidden Access!";
+        
+            return response()->json(['message' => $result]);            
+        }else{
+            $select = [
+                "id",
+                "tpb_id",
+                "kode_tujuan_tpb",
+                "kode AS kode_indikator",
+                "keterangan_tujuan_tpb",
+                "keterangan AS keterangan_kode_indikator"
+            ];
+    
+            $result = KodeIndikator::select($select)->get();
+    
+            return response()->json(['status' => 1, 'message' => 'OK', 'data' => $result]);
+        }
+    }
+
+
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('service-provinsi', 'App\Http\Controllers\ApiController@getprovinsi');
+Route::get('service-kota', 'App\Http\Controllers\ApiController@getkota');
+Route::get('service-bumn-ref', 'App\Http\Controllers\ApiController@getreferensibumnaktif');
+Route::get('service-tpb-ref', 'App\Http\Controllers\ApiController@getreferensitpb');
+Route::get('service-pilar-ref', 'App\Http\Controllers\ApiController@getreferensipilar');
+Route::get('service-kodetujuan-ref', 'App\Http\Controllers\ApiController@getreferensikodetujuantpb');
+Route::get('service-kodeindikator-ref', 'App\Http\Controllers\ApiController@getreferensikodeindikator');
