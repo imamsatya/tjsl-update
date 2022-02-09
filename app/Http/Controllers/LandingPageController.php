3eff24842f05fac7c128d1950d49e1c8b118a7b4
didi.rosidi <didi.rosidi@bumn.go.id>
2022-02-09 14:36:36 +0700

    update  landing page tjsl

--- app/Http/Controllers/LandingPageController.php
+++ app/Http/Controllers/LandingPageController.php
@@ -6,26 +6,26 @@ use Illuminate\Http\Request;
 
 class LandingPageController extends Controller
 {
-    private static $client = null;
+    // private static $client = null;
 
-    public function __construct()
-    {
-        $this->publik_api_host = env('PORTAL_PUBLIK_HOST_API');
-        $this->publik_sso = env('PORTAL_PUBLIK_KODE_SSO');
-        $this->publik_host = env('PORTAL_PUBLIK_HOST');
-    }    
+    // public function __construct()
+    // {
+    //     $this->publik_api_host = env('PORTAL_PUBLIK_HOST_API');
+    //     $this->publik_sso = env('PORTAL_PUBLIK_KODE_SSO');
+    //     $this->publik_host = env('PORTAL_PUBLIK_HOST');
+    // }    
 
-    public function getClient()
-    {
-        if (is_null(self::$client))
-            self::$client = new \GuzzleHttp\Client();
-        return self::$client;
-    }
+    // public function getClient()
+    // {
+    //     if (is_null(self::$client))
+    //         self::$client = new \GuzzleHttp\Client();
+    //     return self::$client;
+    // }
 
     public function index(){
-        $client = self::getClient();
-        $response = $client->get($this->publik_api_host.$this->publik_sso.'/service-to-tjsl');
-        $data = json_decode($response->getBody(), true);
+        // $client = self::getClient();
+        // $response = $client->get($this->publik_api_host.$this->publik_sso.'/service-to-tjsl');
+        // $data = json_decode($response->getBody(), true);
         // return view('landing_page.index-dynamic',['data'=>$data['data'], 'publik_host'=>$this->publik_host]);
         return view('landing_page.index-default');
     }
