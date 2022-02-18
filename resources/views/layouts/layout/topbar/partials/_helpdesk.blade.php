6ef85d4c842feb26396f57ac8e5c42ebbdf94089
didi.rosidi <didi.rosidi@bumn.go.id>
2022-02-18 14:31:07 +0700

    add kontak kami

--- resources/views/layouts/layout/topbar/partials/_helpdesk.blade.php
+++ resources/views/layouts/layout/topbar/partials/_helpdesk.blade.php
@@ -1,11 +1,17 @@
 											
 											<!--begin::Menu-->
-											<div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg menu-state-primary fw-bold py-4 fs-6 w-275px" data-kt-menu="true">
+											<div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-700 menu-state-bg menu-state-primary fw-bold py-4 fs-6 w-275px" data-kt-menu="true">
 												<!--begin::Menu item-->
 												<div class="menu-item px-2">
-													<ul style="list-style-type: none;">
+														<li style="border-bottom:1px;">
+															<a class="menu-link px-5">
+																<strong style="color: blue; cursor:auto;">
+																 Kontak Kami
+																</strong>	
+															</a>
+														</li>
 														<li>
-															<a href="https://wa.me/6287884110762/?text=Hallo+Helpdesk+Portal+TJSL.+Saya+Membutuhkan+bantuan." class="menu-link px-5">
+															<a href="https://wa.me/6287884110762/?text=Hallo+Helpdesk+Portal+TJSL.+Saya+Membutuhkan+bantuan." class="menu-link px-4">
 																<strong>
 																<i class="bi bi-whatsapp" style="color: green;"></i> 
 																&nbsp;Helpdesk TJSL 1
@@ -20,7 +26,14 @@
 																</strong>	
 															</a>
 														</li>
-													</ul>
+														<li>
+															<a href="mailto:asdep.tjsl@bumn.go.id" class="menu-link px-5">
+																<strong>
+																<i class="bi bi-envelope" style="color: green;"></i> 
+																&nbsp;asdep.tjsl@bumn.go.id
+																</strong>	
+															</a>
+														</li>
 												</div>
 												<!--end::Menu item-->
 											</div>
