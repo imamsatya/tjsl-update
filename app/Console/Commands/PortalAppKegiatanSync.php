edf62167e9fce6c98d681affa4b92d87a2bc8f81
didi.rosidi <didi.rosidi@bumn.go.id>
2022-03-14 14:55:21 +0700

    live fixing

--- app/Console/Commands/PortalAppKegiatanSync.php
+++ app/Console/Commands/PortalAppKegiatanSync.php
@@ -94,7 +94,7 @@ class PortalAppKegiatanSync extends Command
                                 'target' => $value->target_bulan? $value->target_bulan : null,               
                                 'realisasi' => $value->realisasi_bulan? $value->realisasi_bulan : 0,            
                                 'anggaran' => $value->anggaran_permintaan? (int) $value->anggaran_permintaan : 0,                     
-                                'anggaran_total' => $value->realisasi_bulan? (int) $value->realisasi_bulan : 0,
+                                'anggaran_total' => $value->realisasi_anggaran_bulan? (int) $value->realisasi_anggaran_bulan : 0,
                                 'status_id' => 1,
                                 'created_at' => $now,
                                 'updated_at' => $now,
@@ -112,7 +112,7 @@ class PortalAppKegiatanSync extends Command
                                 'target' => $value->target_bulan? $value->target_bulan : null,               
                                 'realisasi' => $value->realisasi_bulan? $value->realisasi_bulan : 0,            
                                 'anggaran' => $value->anggaran_permintaan? (int) $value->anggaran_permintaan : 0,                     
-                                'anggaran_total' => $value->realisasi_bulan? (int) $value->realisasi_bulan : 0,
+                                'anggaran_total' => $value->realisasi_anggaran_bulan? (int) $value->realisasi_anggaran_bulan : 0,
                                 'status_id' => 1, //default 
                                 'created_at' => $now,
                                 'updated_at' => $now,
